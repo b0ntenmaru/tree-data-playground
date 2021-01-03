339bc06a833f7e0c150fb22856687bb26890674f
Hiroaki Date <mmst3l4@gmail.com>
2021-01-03 21:24:13 +0900

    server middlewareが部署配列を返すように修正

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,45 @@
 import express from 'express'
 const app = express()
 
-app.get('/', function (_req, res) {
-  res.send('HelloWorld')
+app.get('/departments', function (_req, res) {
+  const departments = [
+    {
+      id: 1,
+      name: '社長室',
+      parent_id: null,
+    },
+    {
+      id: 2,
+      name: 'プロダクトDiv',
+      parent_id: 1,
+    },
+    {
+      id: 3,
+      name: 'エージェントG',
+      parent_id: 2,
+    },
+    {
+      id: 4,
+      name: 'プラットフォームG',
+      parent_id: 2,
+    },
+    {
+      id: 5,
+      name: 'バックエンドG',
+      parent_id: 4,
+    },
+    {
+      id: 6,
+      name: 'リファクタリング部',
+      parent_id: 5,
+    },
+    {
+      id: 7,
+      name: 'フロントエンドG',
+      parent_id: 4,
+    },
+  ]
+  res.send(departments)
 })
 
 module.exports = {
