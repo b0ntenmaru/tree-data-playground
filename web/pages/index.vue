3a1d9c82388abf0381fad5f2acb0144633dadceb
Hiroaki Date <mmst3l4@gmail.com>
2021-01-03 21:31:26 +0900

    バックエンドから受け取った値を表示

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,9 +1,37 @@
 <template>
-  <div class="container">hello</div>
+  <div>{{ departments }}</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from 'vue';
 
-export default Vue.extend({})
+export default Vue.extend({
+  async fetch() {
+    const convertedDepartmentTree = await fetch(
+      'http://localhost:3000/api/departments'
+    ).then((res) => res.json());
+
+    this.departments = this.convertToTree(convertedDepartmentTree);
+  },
+
+  data() {
+    return {
+      departments: null,
+    };
+  },
+
+  methods: {
+    /**
+     * バックエンドから受け取った配列を階層構造データに変換する
+     */
+    convertToTree(datas: any[], id = null, link = 'parent_id') {
+      const rootNode = datas.filter((data) => data[link] === id)
+      const convertedTree: any = rootNode.map((data) => ({
+        ...data,
+        children: this.convertToTree(datas, data.id),
+      }));
+      return convertedTree;
+    },
+  },
+});
 </script>
